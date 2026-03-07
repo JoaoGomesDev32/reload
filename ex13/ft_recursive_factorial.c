2f516f6046a790bba88503d8ca20b7ac606fd709
João Gomes <joaogomesdev32@gmail.com>
2026-03-07 17:52:08 +0000

    ft_recursive_factorial

---
+++ ex13/ft_recursive_factorial.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaog <joaog@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/07 17:40:03 by joaog             #+#    #+#             */
+/*   Updated: 2026/03/07 17:51:17 by joaog            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_factorial(int nb)
+{
+	if (nb < 0)
+		return (0);
+	else if (nb == 0)
+		return (1);
+	return (nb * ft_recursive_factorial(nb -1));
+}
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	printf("%d\n", ft_recursive_factorial(5));
+// 	printf("%d\n", ft_recursive_factorial(0));
+// 	printf("%d\n", ft_recursive_factorial(1));
+// 	printf("%d\n", ft_recursive_factorial(-3));
+// 	return (0);
+// }
