31cf637eff55f8603bc41daf9b790a7501dee9b6
João Gomes <joaogomesdev32@gmail.com>
2026-03-07 17:38:51 +0000

    ft_iterative_factorial

---
+++ ex12/ft_iterative_fatorial.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_fatorial.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joaog <joaog@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/07 17:28:32 by joaog             #+#    #+#             */
+/*   Updated: 2026/03/07 17:38:07 by joaog            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+
+	if (nb < 0)
+		return (0);
+	result = 1;
+	while (nb > 0)
+	{
+		result *= nb;
+		nb--;
+	}
+	return (result);
+}
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	printf("%d\n", ft_iterative_factorial(5));
+// 	printf("%d\n", ft_iterative_factorial(0));
+// 	printf("%d\n", ft_iterative_factorial(1));
+// 	printf("%d\n", ft_iterative_factorial(-3));
+// 	return (0);
+// }
