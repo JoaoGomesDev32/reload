d96c93958e529c109548388f942367c35c535c39
João Gomes <joaogomesdev32@gmail.com>
2026-03-17 13:30:19 +0000

    implementing main

--- ex25/main.c
+++ ex25/main.c
@@ -11,3 +11,15 @@ void	ft_putnbr(int n)
 }
 
 void	ft_foreach(int *tab, int length, void (*f)(int));
+
+int	main(void)
+{
+	int	tab[3];
+
+	tab[0] = 1;
+	tab[1] = 2;
+	tab[2] = 3;
+
+	ft_foreach(tab, 3, &ft_putnbr);
+	return(0);
+}
