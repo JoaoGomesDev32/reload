5a7c148fd484498b7dfcf8cdb264467cca6f1809
João Gomes <joaogomesdev32@gmail.com>
2026-03-21 16:17:23 +0000

    open file

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -12,10 +12,10 @@ int	main(int argc, char **argv)
 		write(2, "File name missing.\n", 19);
 		return (1);
 	}
-
 	if (argc > 2)
 	{
 		write(2, "Too many arguments.\n", 20);
 		return (1);
 	}
+	fd = open(argv[1], O_RDONLY);
 }
