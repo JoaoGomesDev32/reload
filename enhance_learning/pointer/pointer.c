6dfaabd87b3c0d5bf05432c083abef8f725303a3
João Gomes <joaogomesdev32@gmail.com>
2026-03-23 15:28:46 +0000

    entendendo ponteiros CHAR

--- enhance_learning/pointer/pointer.c
+++ enhance_learning/pointer/pointer.c
@@ -13,5 +13,10 @@ int	main(void)
 	printf("Endereço da variável valor2: %p\n", &valor2);
 	printf("Conteúdo da variável valor1: %p\n", valor1);
 	printf("Conteúdo apontado pela variável valor1: %d\n", *valor1);
+
+	printf("\nValor da variável letra2: %c\n", letra2);
+	printf("Endereço da variável letra2: %p\n", &letra2);
+	printf("Conteúdo da variável letra1: %p\n", letra1);
+	printf("Conteúdo apontado pela variável letra1: %c\n", *letra1);
 	return (0);
 }
