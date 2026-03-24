d9660b5b70fc86a3cb9f2b47f3b7b6c68470f24c
João Gomes <joaogomesdev32@gmail.com>
2026-03-24 19:28:50 +0000

    maneiras de criar vetores

---
+++ enhance_learning/array/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	int	idade;
+	char	sexo;
+	float	n1;
+
+	int	num1[10];
+	int	num2[] = {1,5,9,15,43};
+	int	num3[5] = {4,7,5}; //preenche o restante do tamanho com 0,0
+	int	num4[5] = {1,2,3,4,5,6}; //o ultimo indice do vetor sera perdido!
+	int	num5[5] = {0}; // vetor de zeros
+
+	char	letras[100];
+	char	vogais[5] = {'a','e','i','o','u'};
+
+	float	notas[3] = {7.5,8.3,9.5};
+
+	return (0);
+}
