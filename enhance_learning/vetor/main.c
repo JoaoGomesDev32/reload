6dff296408ceb7130da923a59f3db7e6ad01e2ca
João Gomes <joaogomesdev32@gmail.com>
2026-03-24 19:39:40 +0000

    percorrer um vetor

--- enhance_learning/vetor/main.c
+++ enhance_learning/vetor/main.c
@@ -3,6 +3,27 @@
 
 int	main(void)
 {
+	int	i;
+	int	num2[] = {1,5,9,15,43};
+	char	vogais[5] = {'a','e','i','o','u'};
+	float	notas[3] = {7.5,8.3,9.5};
+
+	for(i = 0; i < 5; i++)
+	{
+		printf("%d ", num2[i]);
+	}
+	printf("\n");
+	for(i = 0; i < 5; i++)
+	{
+		printf("%c ", vogais[i]);
+	}
+	printf("\n");
+	for(i = 0; i < 3; i++)
+	{
+		printf("%.2f ", notas[i]);
+	}
+
+	/*
 	int	idade;
 	char	sexo;
 	float	n1;
@@ -17,6 +38,6 @@ int	main(void)
 	char	vogais[5] = {'a','e','i','o','u'};
 
 	float	notas[3] = {7.5,8.3,9.5};
-
+	*/
 	return (0);
 }
