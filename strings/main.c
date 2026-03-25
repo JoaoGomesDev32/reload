045fa86a1e90bcd07b94d077b8b8b1056468c5c6
João Gomes <joaogomesdev32@gmail.com>
2026-03-25 18:43:29 +0000

    introdição strings

---
+++ strings/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	char	palavras[] = {"Olá vamos aprender a programar com a linguagem C?"};
+
+	printf("%s", palavras);
+
+	return (0);
+}
